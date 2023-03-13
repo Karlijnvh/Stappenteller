d5657257dd2a328ac89e68fd1849710169b738d4
Karlijnvh <112547700+Karlijnvh@users.noreply.github.com>
2023-03-13 10:00:18 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -4,29 +4,30 @@ function berekenVersnelling () {
     z = input.acceleration(Dimension.Z)
     versnelling = Math.floor(Math.sqrt(x * x + y * y + z * z))
 }
+let bewegen = 0
 let versnelling = 0
 let z = 0
 let y = 0
 let x = 0
 let stilstaand = 0
-let aantalStappen = 0
 let toestand = stilstaand
 let onderDrempel = 1024
-let bovenDrempel = 2048
+let bovenDrempel = 1048
+let aantalStappen = 0
 basic.forever(function () {
     berekenVersnelling()
     serial.writeValue("versnelling", versnelling)
 })
 basic.forever(function () {
-    if (input.acceleration(Dimension.X) > 2048) {
-        let bewegen = 0
+    if (versnelling > bovenDrempel) {
         toestand = bewegen
-    } else {
-        if (input.acceleration(Dimension.X) < 1024) {
-            toestand = stilstaand
-        }
     }
 })
 basic.forever(function () {
     basic.showString("" + (aantalStappen))
+})
+basic.forever(function () {
+    if (toestand == bewegen) {
+        aantalStappen += aantalStappen + 1
+    }
 })
