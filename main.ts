d17c70d7ad925ad4c8ef6330297410d513dcbc11
Karlijnvh <112547700+Karlijnvh@users.noreply.github.com>
2023-03-13 09:29:58 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
 let stilstaand = 0
-let toestand = stilstaand
 let aantalStappen = 0
+let toestand = stilstaand
 let onderDrempel = 1024
 let bovenDrempel = 2048
+basic.forever(function () {
+    if (input.acceleration(Dimension.X) > 2048) {
+        let bewegen = 0
+        toestand = bewegen
+    } else {
+        if (input.acceleration(Dimension.X) < 1024) {
+            toestand = stilstaand
+        }
+    }
+})
