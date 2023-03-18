0b6823be3f709161156f68fd065385a94c191775
Karlijnvh <112547700+Karlijnvh@users.noreply.github.com>
2023-03-18 14:15:07 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -2,21 +2,32 @@ function berekenVersnelling () {
     x = input.acceleration(Dimension.X)
     y = input.acceleration(Dimension.Y)
     z = input.acceleration(Dimension.Z)
-    versnelling = Math.floor(Math.sqrt(x * x + y * y + z * z))
+    versnelling = Math.floor(Math.sqrt(x ** 2 + y ** 2 + z ** 2))
+    basic.pause(500)
 }
-let bewegen = 0
+input.onButtonPressed(Button.AB, function () {
+    aantalStappen = 0
+})
 let versnelling = 0
 let z = 0
 let y = 0
 let x = 0
+let aantalStappen = 0
 let stilstaand = 0
 let toestand = stilstaand
-let onderDrempel = 1024
-let bovenDrempel = 2048
-let aantalStappen = 0
+let onderDrempel = 700
+let bovenDrempel = 1023
+aantalStappen = 0
 basic.forever(function () {
+    let bewegen = 0
     if (toestand == stilstaand && versnelling > bovenDrempel) {
         toestand = bewegen
+        aantalStappen += 1
+    } else {
+        if (toestand == bewegen && versnelling < onderDrempel) {
+            toestand = stilstaand
+            aantalStappen = aantalStappen
+        }
     }
 })
 basic.forever(function () {
@@ -25,9 +36,5 @@ basic.forever(function () {
 })
 basic.forever(function () {
     basic.showNumber(aantalStappen)
-})
-basic.forever(function () {
-    if (toestand == bewegen) {
-        aantalStappen += aantalStappen + 1
-    }
+    basic.pause(100)
 })
